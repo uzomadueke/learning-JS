89cff3051b2b4b7117a6124d0a3cf24282efa89f
Uzo Madueke <uzo_madueke@yahoo.com>
2025-03-03 15:38:56 +0100

    edited the footer and updated my projects

--- src/App.js
+++ src/App.js
@@ -16,23 +16,24 @@ function App() {
       <section id="about">
         <h2>About Kennedy Madueke</h2>
         <p>
-          I am a software engineer with a strong foundation in computer science principles, 
-          dedicated to solving real-world problems through innovative software solutions. 
+          I am a software engineer with a strong foundation in computer science principles, dedicated to solving real-world problems through innovative software solutions. 
           Constantly eager to explore new technologies, I thrive in both personal and collaborative projects that push the boundaries of creativity and functionality. 
-          My ability to adapt and take initiative has been shaped by hands-on experience, 
-          while my strong communication skills ensure clarity in code, documentation, and technical reports. 
+          My ability to adapt and take initiative has been shaped by hands-on experience, while my strong communication skills ensure clarity in code, documentation, and technical reports. 
           Through my work, I aim to blend technical excellence with creativity, building solutions that are both impactful and engaging.
           </p>
       </section>
       <section id="projects">
         <h2>My Projects</h2>
         <ul>
-          <li>Project 1</li>
-          <li>Project 2</li>
+          <li>THIS WEBSITE</li>
+          <li>SPELLING GAME APP</li>
+          <li>AI CAR SIMULATION</li>
+          <li>HANGMAN</li>
         </ul>
       </section>
       <footer>
         <p>Contact us at ss.socialstudios@outlook.com</p>
+        <p>MADUEKE'S PORTFOLIO. All Right Reserved. Developed by socialstudios.</p>
       </footer>
     </div>
   );
